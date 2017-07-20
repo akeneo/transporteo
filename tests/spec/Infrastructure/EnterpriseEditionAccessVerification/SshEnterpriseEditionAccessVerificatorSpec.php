36f07f3e526eb49d10ef5cf0acc56c91e3ea8069
Anael CHARDAN <anael.chardan@gmail.com>
2017-07-20 13:19:27 -0400

    TIP-773: SSH

---
+++ tests/spec/Infrastructure/EnterpriseEditionAccessVerification/SshEnterpriseEditionAccessVerificatorSpec.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Akeneo\PimMigration\Infrastructure\EnterpriseEditionAccessVerification;
+
+use Akeneo\PimMigration\Domain\SourcePimDetection\SourcePim;
+use Akeneo\PimMigration\Infrastructure\EnterpriseEditionAccessVerification\SshEnterpriseEditionAccessVerificator;
+use PhpSpec\ObjectBehavior;
+
+/**
+ * Spec for SSH enterprise edition access verificator.
+ *
+ * @author    Anael Chardan <anael.chardan@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ */
+class SshEnterpriseEditionAccessVerificatorSpec extends ObjectBehavior
+{
+    public function it_is_initializable()
+    {
+        $this->shouldHaveType(SshEnterpriseEditionAccessVerificator::class);
+    }
+
+    public function it_should(SourcePim $sourcePim)
+    {
+        $sourcePim->getEnterpriseRepository()->willReturn('ssh://git@distribution.akeneo.com:443/pim-enterprise-dev-nanou-migration.git');
+        $this->verify($sourcePim);
+    }
+}

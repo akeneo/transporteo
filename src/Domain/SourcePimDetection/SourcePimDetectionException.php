3205f1cea7b6119c7037e31fd7019d67509bd712
Anael CHARDAN <anael.chardan@gmail.com>
2017-07-20 09:48:57 -0400

    TIP-772: Fix PR issues

--- src/Domain/SourcePimDetection/SourcePimDetectionException.php
+++ src/Domain/SourcePimDetection/SourcePimDetectionException.php
@@ -10,6 +10,6 @@ namespace Akeneo\PimMigration\Domain\SourcePimDetection;
  * @author    Anael Chardan <anael.chardan@akeneo.com>
  * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
  */
-class NotAcceptablePimException extends \Exception
+class SourcePimDetectionException extends \Exception
 {
 }

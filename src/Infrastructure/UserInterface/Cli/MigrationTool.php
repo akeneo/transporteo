98ad762a1fd23719cc226953e3209574f10a2b04
Anael CHARDAN <anael.chardan@gmail.com>
2017-07-21 16:14:03 -0400

    TIP-794: Start console question

--- src/Infrastructure/UserInterface/Cli/MigrationTool.php
+++ src/Infrastructure/UserInterface/Cli/MigrationTool.php
@@ -2,15 +2,21 @@
 
 namespace Akeneo\PimMigration\Infrastructure\UserInterface\Cli;
 
+use Akeneo\PimMigration\Domain\EnterpriseEditionAccessVerification\EnterpriseEditionAccessException;
 use Akeneo\PimMigration\Domain\SourcePimConfiguration\PimServerInformation;
 use Akeneo\PimMigration\Domain\SourcePimConfiguration\SourcePimConfigurator;
 use Akeneo\PimMigration\Domain\SourcePimDetection\SourcePim;
 use Akeneo\PimMigration\Infrastructure\EnterpriseEditionAccessVerification\SshEnterpriseEditionAccessVerificator;
 use Akeneo\PimMigration\Infrastructure\LocalFileFetcher;
-use resources\Akeneo\PimMigration\ResourcesFileLocator;
+use Akeneo\PimMigration\Infrastructure\ServerAccessInformation;
+use Akeneo\PimMigration\Infrastructure\SshFileFetcher;
+use Akeneo\PimMigration\Infrastructure\SshKey;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\QuestionHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ChoiceQuestion;
+use Symfony\Component\Console\Question\Question;
 
 final class MigrationTool extends Command
 {
@@ -25,14 +31,49 @@ final class MigrationTool extends Command
     {
         $output->writeln('Akeneo Pim Migration tool');
 
-        //StepTwo
-        $output->writeln('Source Pim Configuration: Collect your configuration files');
-        $pimServerInformation = new PimServerInformation(
-            ResourcesFileLocator::getStepOneAbsoluteComposerJsonLocalPath(),
-            'nanou-project'
-        );
+        /** @var QuestionHelper $helper */
+        $helper = $this->getHelper('question');
 
-        $sourcePimConfigurator = new SourcePimConfigurator(new LocalFileFetcher());
+        $output->writeln('Source Pim Configuration: Collect your configuration files');
+
+        $projectNameQuestion = new Question('What is the name of the project you want to migrate? ');
+        $projectName = $helper->ask($input, $output, $projectNameQuestion);
+
+        $pimLocationQuestion = new ChoiceQuestion('Where is located your PIM? ', ['local', 'server']);
+
+        $pimLocation = $helper->ask($input, $output, $pimLocationQuestion);
+
+        $fetcher = null;
+        $composerJsonPathQuestion = null;
+        $sshKeySourcePimServer = null;
+
+        if ('server' === $pimLocation) {
+            $hostQuestion = new Question('What is the hostname of the source PIM server? ');
+            $host = $helper->ask($input, $output, $hostQuestion);
+
+            $portQuestion = new Question('What is the SSH port of the source PIM server? ', 22);
+            $port = $helper->ask($input, $output, $portQuestion);
+
+            $userNameQuestion = new Question('What is the SSH user you want to connect with ? ');
+            $user = $helper->ask($input, $output, $userNameQuestion);
+
+            $sshKeyPathQuestion = new Question('Where is located the private SSH key able to connect to the server ? ');
+            $sshPath = $helper->ask($input, $output, $sshKeyPathQuestion);
+
+            $sshKeySourcePimServer = new SshKey($sshPath);
+            $serverAccess = new ServerAccessInformation($host, $port, $user, $sshKeySourcePimServer);
+            $fetcher = new SshFileFetcher($serverAccess);
+
+            $composerJsonPathQuestion = new Question('Where is located the composer.json on the server? ');
+        } else {
+            $fetcher = new LocalFileFetcher();
+            $composerJsonPathQuestion = new Question('Where is located the composer.json on your computer? ');
+        }
+
+        $composerJsonPath = $helper->ask($input, $output, $composerJsonPathQuestion);
+        $pimServerInformation = new PimServerInformation($composerJsonPath, $projectName);
+
+        $sourcePimConfigurator = new SourcePimConfigurator($fetcher);
         $sourcePimConfiguration = $sourcePimConfigurator->configure($pimServerInformation);
 
         //StepThree
@@ -49,10 +90,34 @@ final class MigrationTool extends Command
         //Step four
         if ($sourcePim->isEnterpriseEdition()) {
             $output->writeln('EnterpriseEdition Access Verification');
-            $sshVerificator = new SshEnterpriseEditionAccessVerificator();
 
-            $sshVerificator->verify($sourcePim, $sourcePimConfiguration->getSshKey());
-            $output->writeln('Access to the Enterprise Edition allowed');
+            $sshKeyEnterpriseEditionServer = null;
+
+            if (null !== $sshKeySourcePimServer) {
+                $sshVerificator = new SshEnterpriseEditionAccessVerificator($sshKeySourcePimServer);
+                try {
+                    $sshVerificator->verify($sourcePim);
+                } catch (EnterpriseEditionAccessException $exception) {
+                    $output->writeln('You are not allowed to connect to the EnterpriseEdition server with the SSH key you give us already.');
+                }
+
+                $sshKeyPathQuestion = new Question('Where is located your SSH key allowed to connect to Akeneo Enterprise Edition distibution? ');
+                $sshPath = $helper->ask($input, $output, $sshKeyPathQuestion);
+                $sshKey = new SshKey($sshPath);
+                $sshVerificator = new SshEnterpriseEditionAccessVerificator($sshKey);
+                $sshVerificator->verify($sourcePim);
+
+            } else {
+
+                $sshKeyPathQuestion = new Question('Where is located your SSH key allowed to connect to Akeneo Enterprise Edition distibution? ');
+                $sshPath = $helper->ask($input, $output, $sshKeyPathQuestion);
+                $sshKey = new SshKey($sshPath);
+                $sshVerificator = new SshEnterpriseEditionAccessVerificator($sshKey);
+                $sshVerificator->verify($sourcePim);
+            }
+            $output->writeln('Access to the Enterprise Edition allowed.');
         }
+
+        $output->writeln('Migration finished');
     }
 }

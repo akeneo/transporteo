f46c8442d09c00e04e2ea25154ff73fdd486b4dd
Anael CHARDAN <anael.chardan@gmail.com>
2017-08-22 00:33:04 -0400

    MIG-2: Migrate system data

---
+++ src/Domain/SystemMigration/SystemMigrationException.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\PimMigration\Domain\StructureMigration;
+
+use Akeneo\PimMigration\Domain\MigrationStepException;
+use Throwable;
+
+/**
+ * Exception for the system tables migration.
+ *
+ * @author    Anael Chardan <anael.chardan@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ */
+class SystemMigrationException extends MigrationStepException
+{
+    public function __construct($message = '', $code = 0, Throwable $previous = null)
+    {
+        $message = sprintf('Error: Step 10 - System Migration: %s', $message);
+
+        parent::__construct($message, $code, $previous);
+    }
+}

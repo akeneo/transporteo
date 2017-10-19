be7e671633a24a8286c8a2d29ed48f946d2f1269
Laurent Petard <laurent.petard@akeneo.com>
2017-10-19 16:20:19 +0200

    MIG-36: merge IVB and variant-group migration

---
+++ src/Domain/MigrationStep/s150_ProductVariationMigration/ProductVariationMigrationException.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\PimMigration\Domain\MigrationStep\s150_ProductVariationMigration;
+
+use Akeneo\PimMigration\Domain\MigrationStep\MigrationStepException;
+
+/**
+ * Exception for the migration of the product variations.
+ *
+ * @author    Laurent Petard <laurent.petard@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ */
+class ProductVariationMigrationException extends MigrationStepException
+{
+    public function __construct(string $message)
+    {
+        parent::__construct('Error: Step 15 - Product variation migration: '.$message);
+    }
+}
---
+++ src/Domain/MigrationStep/s150_ProductVariationMigration/ProductVariationMigrator.php
@@ -0,0 +1,122 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Akeneo\PimMigration\Domain\MigrationStep\s150_ProductVariationMigration;
+
+use Akeneo\PimMigration\Domain\Command\ChainedConsole;
+use Akeneo\PimMigration\Domain\Command\SymfonyCommand;
+use Akeneo\PimMigration\Domain\DataMigration\DataMigrator;
+use Akeneo\PimMigration\Domain\Pim\DestinationPim;
+use Akeneo\PimMigration\Domain\Pim\SourcePim;
+use Psr\Log\LoggerInterface;
+
+/**
+ * Migrates products variations (via IVB and variant-group).
+ *
+ * @author    Laurent Petard <laurent.petard@akeneo.com>
+ * @copyright 2017 Akeneo SAS (http://www.akeneo.com)
+ */
+class ProductVariationMigrator implements DataMigrator
+{
+    const MAX_VARIANT_AXES = 5;
+
+    const ALLOWED_AXE_TYPES = [
+        'pim_catalog_simpleselect',
+        'pim_reference_data_simpleselect',
+        'pim_catalog_metric',
+        'pim_catalog_boolean',
+    ];
+
+    /** @var InnerVariationTypeMigrator */
+    private $innerVariantTypeMigrator;
+
+    /** @var VariantGroupMigrator */
+    private $variantGroupMigrator;
+
+    /** @var ChainedConsole */
+    private $console;
+
+    /** @var VariantGroupRetriever */
+    private $variantGroupRetriever;
+
+    /** @var LoggerInterface */
+    private $logger;
+
+    public function __construct(
+        ChainedConsole $console,
+        InnerVariationTypeMigrator $innerVariantTypeMigrator,
+        VariantGroupMigrator $variantGroupMigrator,
+        VariantGroupRetriever $variantGroupRetriever,
+        LoggerInterface $logger
+    ) {
+        $this->console = $console;
+        $this->innerVariantTypeMigrator = $innerVariantTypeMigrator;
+        $this->variantGroupMigrator = $variantGroupMigrator;
+        $this->variantGroupRetriever = $variantGroupRetriever;
+        $this->logger = $logger;
+    }
+
+    public function migrate(SourcePim $sourcePim, DestinationPim $destinationPim): void
+    {
+        $warnings = [];
+
+        try {
+            $isIvbMigrationDone = $this->migrateInnerVariationBundle($sourcePim, $destinationPim);
+        } catch (InvalidInnerVariationTypeException $exception) {
+            $warnings[] = $exception->getMessage();
+            $isIvbMigrationDone = true;
+        }
+
+        try {
+            $isVariantGroupMigrationDone = $this->migrateVariantGroups($sourcePim, $destinationPim);
+        } catch (InvalidVariantGroupException $exception) {
+            $warnings[] = $exception->getMessage();
+            $isVariantGroupMigrationDone = true;
+        }
+
+        if ($isIvbMigrationDone || $isVariantGroupMigrationDone) {
+            $this->refreshElasticSearchIndexes($destinationPim);
+        }
+
+        if (!empty($warnings)) {
+            throw new InvalidProductVariationException($warnings);
+        }
+    }
+
+    private function migrateInnerVariationBundle(SourcePim $sourcePim, DestinationPim $destinationPim): bool
+    {
+        if (!$sourcePim->hasIvb()) {
+            $this->logger->info('There is no InnerVariationType to migrate.');
+
+            return false;
+        }
+
+        $this->innerVariantTypeMigrator->migrate($sourcePim, $destinationPim);
+
+        return true;
+    }
+
+    private function migrateVariantGroups(SourcePim $sourcePim, DestinationPim $destinationPim): bool
+    {
+        $numberOfVariantGroups = $this->variantGroupRetriever->retrieveNumberOfVariantGroups($destinationPim);
+
+        if (0 === $numberOfVariantGroups) {
+            $this->logger->info("There are no variant groups to migrate");
+
+            return false;
+        }
+
+        $this->logger->info(sprintf("There are %d variant groups to migrate", $numberOfVariantGroups));
+
+        $this->variantGroupMigrator->migrate($sourcePim, $destinationPim);
+
+        return true;
+    }
+
+    private function refreshElasticSearchIndexes(DestinationPim $destinationPim)
+    {
+        $this->console->execute(new SymfonyCommand('pim:product:index --all', SymfonyCommand::PROD), $destinationPim);
+        $this->console->execute(new SymfonyCommand('pim:product-model:index --all', SymfonyCommand::PROD), $destinationPim);
+    }
+}
